1eb7b452c3ce06d8f8ff181cd53bfbd8f9c3e1f6
= <=>
2024-05-16 19:17:27 +0900

    :sparkles: FEAT. Login을 위한 loginRequestDto 작성

--- src/main/java/com/example/account/dto/MemberRequestDto.java
+++ src/main/java/com/example/account/dto/MemberRequestDto.java
@@ -4,18 +4,33 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
-@Getter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
 public class MemberRequestDto {
-    @NotBlank(message = "로그인 아이디를 입력해주세요.")
-    private String userId;
 
-    @NotBlank(message = "비밀번호를 입력해주세요.")
-    private String password;
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class signUpRequestDto{
+        @NotBlank(message = "로그인 아이디를 입력해주세요.")
+        private String userId;
 
-    @Email(message = "이메일 형식을 맞춰주세요.")
-    @NotBlank
-    private String email;
+        @NotBlank(message = "비밀번호를 입력해주세요.")
+        private String password;
+
+        @Email(message = "이메일 형식을 맞춰주세요.")
+        @NotBlank
+        private String email;
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class loginRequestDto{
+        @NotBlank(message = "로그인 아이디를 입력해주세요.")
+        private String userId;
+
+        @NotBlank(message = "비밀번호를 입력해주세요.")
+        private String password;
+    }
 }
