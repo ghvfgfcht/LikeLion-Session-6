6ad587bbd56348e9908a74917f35d74f3d579457
= <=>
2024-05-16 19:17:57 +0900

    :sparkles: FEAT. Login을 위한 login 메서드 추가

--- src/main/java/com/example/account/service/MemberService.java
+++ src/main/java/com/example/account/service/MemberService.java
@@ -5,5 +5,8 @@ import com.example.account.util.response.CustomApiResponse;
 import org.springframework.http.ResponseEntity;
 
 public interface MemberService {
-    ResponseEntity<CustomApiResponse<?>> signUp(MemberRequestDto member);
+
+    ResponseEntity<CustomApiResponse<?>> signUp(MemberRequestDto.signUpRequestDto member);
+
+    ResponseEntity<CustomApiResponse<?>> login(MemberRequestDto.loginRequestDto member);
 }
