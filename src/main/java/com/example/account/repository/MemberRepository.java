cb54c70e5a6dd674e95d355d8545a5babef8932f
= <=>
2024-05-16 19:17:00 +0900

    :sparkles: FEAT. Login을 위한 findByUserId Jpa 메서드 작성

--- src/main/java/com/example/account/repository/MemberRepository.java
+++ src/main/java/com/example/account/repository/MemberRepository.java
@@ -3,6 +3,8 @@ package com.example.account.repository;
 import com.example.account.entity.Members;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Members, Long> {
+import java.util.Optional;
 
+public interface MemberRepository extends JpaRepository<Members, Long> {
+    Optional<Members> findByUserId(String userId);
 }
